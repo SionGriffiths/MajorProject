be59ccc5f271f48659085ad337d201db100c4ac7
s.griffiths <sig2@aber.ac.uk>
2016-02-24 16:13:39 +0000

    PA-10 - Test commit from Intellij, test CI build with unit test, commit simple sample test

---
+++ NppcDataVisualiser/src/test/java/HomePageControllerWebTest.java
@@ -0,0 +1,46 @@
+import com.siongriffiths.nppcdatavisualiser.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created on 24/02/2016.
+ *
+ * @author Siôn Griffiths / sig2@aber.ac.uk
+ *
+ * Basic test, based on examples given in https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = Application.class)
+public class HomePageControllerWebTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testHome() throws Exception {
+        this.mockMvc.perform(get("/")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("<title>DEFAULT HOME")));;
+    }
+
+}
