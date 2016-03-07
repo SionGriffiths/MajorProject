2adc13df09d15fef0364164048ad7ff90fb34422
s.griffiths <sig2@aber.ac.uk>
2016-03-07 20:46:06 +0000

    VPM-18 - Adding plantImage dao and service

--- NppcDataVisualiser/src/main/java/com/siongriffiths/nppcdatavisualiser/plants/daos/PlantImageDao.java
+++ NppcDataVisualiser/src/main/java/com/siongriffiths/nppcdatavisualiser/plants/daos/PlantImageDao.java
@@ -2,31 +2,15 @@ package com.siongriffiths.nppcdatavisualiser.plants.daos;
 
 import com.siongriffiths.nppcdatavisualiser.plants.PlantImage;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
- * Created on 27/02/2016.
+ * Created on 07/03/2016.
  *
  * @author Siôn Griffiths / sig2@aber.ac.uk
  */
+public interface PlantImageDao {
 
-@Repository
-@Transactional
-public class PlantImageDao {
+    void savePlantImage(PlantImage plantImage);
 
-    @Autowired
-    private SessionFactory sessionFactory;
-
-
-    public void savePlantImage(PlantImage plantImage){
-        getSession().saveOrUpdate(plantImage);
-    }
-
-    private Session getSession() {
-        return sessionFactory.getCurrentSession();
-    }
 
 }
---
+++ NppcDataVisualiser/src/main/java/com/siongriffiths/nppcdatavisualiser/plants/service/PlantImageManagerImpl.java
@@ -0,0 +1,15 @@
+package com.siongriffiths.nppcdatavisualiser.plants.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created on 07/03/2016.
+ *
+ * @author Siôn Griffiths / sig2@aber.ac.uk
+ */
+@Service("plantImageManager")
+public class PlantImageManagerImpl implements PlantImageManager {
+
+
+
+}
