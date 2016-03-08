573d07da3d0f6fdce174a75944f90902f305471c
s.griffiths <sig2@aber.ac.uk>
2016-03-08 17:53:45 +0000

    VPM-13 - Add environment specific image repo directory mapping to serve static content from disk

---
+++ NppcDataVisualiser/src/main/java/com/siongriffiths/nppcdatavisualiser/configuration/CustomWebMvcAutoConfig.java
@@ -0,0 +1,35 @@
+package com.siongriffiths.nppcdatavisualiser.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+/**
+ * Created on 08/03/2016.
+ *
+ * @author Siôn Griffiths / sig2@aber.ac.uk
+ *
+ *
+ * The code in this class is adapted from http://stackoverflow.com/questions/21123437/how-do-i-use-spring-boot-to-serve-static-content-located-in-dropbox-folder
+ */
+@Configuration
+@AutoConfigureAfter(DispatcherServletAutoConfiguration.class)
+public class CustomWebMvcAutoConfig extends
+        WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter {
+
+    @Value("${image-repository.root}")
+    private String imageRepoRoot;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String myExternalFilePath = imageRepoRoot;
+
+        registry.addResourceHandler("/m/**").addResourceLocations(myExternalFilePath);
+
+        super.addResourceHandlers(registry);
+    }
+
+}
