a40934389b75c8ddf4a96b384dbf16eb3fc25017
s.griffiths <sig2@aber.ac.uk>
2016-03-10 10:13:00 +0000

    VPM-20 - Add ajax update of tags(shonky v1)

--- NppcDataVisualiser/src/main/resources/static/js/plantDetails.js
+++ NppcDataVisualiser/src/main/resources/static/js/plantDetails.js
@@ -10,21 +10,21 @@ $( document ).ready(function() {
             type: 'post',
             data: $form.serialize(),
             success: function(response) {
-                // if the response contains any errors, replace the form
-                console.log(response);
-                console.log($form);
-                console.log($form.closest(".col-xs-6").children(".tagTextDiv"));
-                console.log($form.parent().children());
-                console.log($form.parent());
-                $form.parent().siblings(".plant_day_tags").html("DICK DICK DICKHOOOOLE!");
-                if ($(response).find('.has-error').length) {
-                    $form.replaceWith(response);
-                } else {
-                    // in this case we can actually replace the form
-                    // with the response as well, unless we want to
-                    // show the success message a different way
-                }
+
+              var targetElem =  $form.closest(".col-xs-6").children(".plant_day_tags");
+               targetElem.html(parseReponseToHtml(response));
             }
         });
     });
+
+
+    function parseReponseToHtml(jsonResponse){
+        var htmlString = "";
+        $(jsonResponse).each(function(){
+            console.log(this.tagContent);
+          htmlString += "<span class='tag_text'>"+this.tagContent+"</span> ";
+        });
+        return htmlString;
+    }
+
 });
