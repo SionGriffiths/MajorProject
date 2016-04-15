01aaf54abf82d4856adae3b73f0e45876e3b8e56
s.griffiths <sig2@aber.ac.uk>
2016-04-15 22:46:47 +0100

    Configuring some latex bits

---
+++ Documentation/Final Report/sourceCode/mySnippets/testing/testShowPlants.java
@@ -0,0 +1,9 @@
+@Test
+public void testShowPlants() throws Exception {
+    String testBarCode = "bc1";
+    this.mockMvc.perform(get("/plants").sessionAttrs(sessionattr))
+              .andDo(print())
+              .andExpect(status().isOk())
+              .andExpect(content().string(containsString(testBarCode)))
+              .andExpect(view().name("plants/show"));
+}
